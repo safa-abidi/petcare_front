0c2b48cd2988c53c09af76c7024cee55203131cc
Amal1999 <sammari_@outlook.fr>
2022-11-24 10:07:26 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { DropdownWithIconComponent } from './form/dropdown-with-icon/dropdown-with-icon.component';
+import { TextInputWithIconComponent } from './form/text-input-with-icon/text-input-with-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignupComponent,
+    DropdownWithIconComponent,
+    TextInputWithIconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
