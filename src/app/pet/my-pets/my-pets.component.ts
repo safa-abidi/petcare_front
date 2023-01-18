918eeaf6c61c7d3c4e88464400f9e0ed34a5551a
Amal1999 <sammari_a@outlook.fr>
2023-01-18 01:43:25 +0100

    finished

--- src/app/pet/my-pets/my-pets.component.ts
+++ src/app/pet/my-pets/my-pets.component.ts
@@ -9,36 +9,53 @@ import { PetService } from 'src/app/services/pet.service';
 })
 export class MyPetsComponent implements OnInit {
 
+  filter: string = "";
   myPets: Pet[] = []
-  currentDate : Date = new Date();
+  filtered : Pet[] = []
+  currentDate: Date = new Date();
 
   // localStorage.getItem("userId")
   constructor(private petService: PetService) {
-    petService.getMyPets(localStorage.getItem("userId")!).subscribe((result) => {
-      console.log(result.length);
-      this.myPets = result;
-    });
+
   }
-  
+
 
   ngOnInit(): void {
+    this.getPets();
+  }
+  getPets() {
+    this.petService.getMyPets(localStorage.getItem("userId")!).subscribe((result) => {
+      console.log(result.length);
+      this.myPets = result;
+      this.filtered  = result;
+
+
+    });
   }
 
 
-  getAge( birthDate : string){
+  getAge(birthDate: string) {
     const ageInMilliseconds = Date.now() - Date.parse(birthDate);
     return ageInMilliseconds / (1000 * 60 * 60 * 24 * 365);
   }
 
-  public searchCats(key: string): void {
-    console.log(key);
+  public searchPets(event : any) {
+
+    console.log("hell")
+    console.log(event.target.value);
+    
+    const key :string = event.target.value;
     const results: Pet[] = [];
-    for (const employee of this.myPets) {
-      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      || employee.type.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
-        results.push(employee);
-      }
+    for (const pet of this.myPets) {
+      if (pet.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 || pet.breed.toLowerCase().indexOf(key.toLowerCase()) !== -1 ) { results.push(pet); }
     }
+    console.log(results.length);
+    
+    this.filtered = results;
+    if (results.length === 0 || key == "") {
+      this.getPets();
+    }
+
   }
 
 }
